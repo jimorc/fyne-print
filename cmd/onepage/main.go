3f38b1e12fd8b625670e8e40e1c272ef28055a39
Jim <jimorcheson@gmail.com>
2025-02-11 12:28:26 -0500

    Add some content to the main window
    If we don't, the menu may or may not display.

--- cmd/onepage/main.go
+++ cmd/onepage/main.go
@@ -5,6 +5,7 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
@@ -21,6 +22,9 @@ func main() {
 
 	mainMenu := fyne.NewMainMenu(fileMenu)
 	w.SetMainMenu(mainMenu)
+	// we need some content in the main window or we get some
+	// weird results (menu displays or doesn't)
+	w.SetContent(widget.NewLabel("Put something in the window"))
 	w.Resize(fyne.NewSize(800, 600))
 	w.ShowAndRun()
 }
