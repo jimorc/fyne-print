47e61ef8287152f52a3dcb47ff8fd2fd9f80a5f1
Jim <jimorcheson@gmail.com>
2025-03-05 11:54:42 -0500

    Update StringFromUTF16 function comment
    More correctly identifies the argument type.

--- print/utils_windows.go
+++ print/utils_windows.go
@@ -10,7 +10,7 @@ import (
 // maxUTF16Len is the maximum length of a UTF16 slice.
 const maxUTF16Len = 1024
 
-// StringFromUTF16 converts a uint16 pointer to a string.
+// StringFromUTF16 converts a pointer to a slice of UTF16 characters to a string.
 // The maximum length of the UTF16 slice is maxUTF16Len as that should be more than enough for any
 // UTF16 string returned from win32 printer functions.
 func StringFromUTF16(utf16 *uint16) string {
