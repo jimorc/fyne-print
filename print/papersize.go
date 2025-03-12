20ae60e2fbb077bdeb73669aa4978ea40869cf82
Jim Orcheson <jimorcheson@gmail.com>
2025-03-12 14:12:45 -0400

    Add PaperSize and PaperSizes structs
    stdPaperSizes contains the complete list of IANA registered IPP media sizes.
    newPaperSize creates a PaperSize object.

---
+++ print/papersize.go
@@ -0,0 +1,36 @@
+package print
+
+// Margin is the paper margin. That is, the margins that will not be printed.
+type Margin struct {
+	top    float32
+	bottom float32
+	left   float32
+	right  float32
+}
+
+// NewMargin creates a Margin struct for the values input.
+func NewMargin(top, bottom, left, right float32) Margin {
+	return Margin{top: top, bottom: bottom, left: left, right: right}
+}
+
+// Papersize defines the size of the paper.
+type PaperSize struct {
+	psName string
+	name   string
+	width  float32
+	height float32
+}
+
+// newPaperSize creates a PaperSize struct for the values input.
+//
+// Params:
+//	psName is the name of the paper size. This would normally be the IPP media size name
+// registered with IANA. If the media size is not registered, it may be some other name.
+//	name is the English name of the media size. It is translated to the system language if the
+// translation exists.
+//	width is the width in 100ths of a mm for the media in portrait mode.
+//	height is the height in 100ths of a mm for the media in portrait mode.
+func newPaperSize(psName string, name string, width float32, height float32) PaperSize {
+	// TODO: translate name string to system language using fyne lang module.
+	return PaperSize{psName: psName, name: name, width: width, height: height}
+}
